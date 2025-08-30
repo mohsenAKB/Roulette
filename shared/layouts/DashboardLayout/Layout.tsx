436db37039f382748c0f6f21478ac825eb2ae191
mohsenAKB <mohsen.akb1377@gmail.com>
2025-08-30 13:21:29 +0330

    [UPDATE]: move header and footer components to their subfolders

--- shared/layouts/DashboardLayout/Layout.tsx
+++ shared/layouts/DashboardLayout/Layout.tsx
@@ -1,8 +1,8 @@
 import React, { FC, ReactNode } from 'react'
 
 import { LayoutProps } from '@/shared/types/layout'
-import Header from './Header'
-import Footer from './Footer'
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
 import Main from './Main'
 
 const Layout: FC<LayoutProps> = ({
