e19c781137deb79f88eaf24cce1b20cf3b2fc97b
mohsenAKB <mohsen.akb1377@gmail.com>
2025-08-30 20:09:40 +0330

    [CREATE]: styles of menu items in header

--- shared/layouts/DashboardLayout/Header/Menu/MenuIItem.tsx
+++ shared/layouts/DashboardLayout/Header/Menu/MenuIItem.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from 'react'
 
 import { MenuItem as MenuItemProps } from "@/shared"
 import Image from 'next/image'
+import ArrowIcon from './ArrowIcon'
 
 interface Props extends MenuItemProps { }
 
@@ -14,18 +15,13 @@ const MenuItem: FC<Props> = ({
 
   const renderArrowIcon = (): ReactNode => {
     if (items) {
-      return <Image
-        src="\icons\chevron-down.svg"
-        alt='arrow'
-        width={20}
-        height={20}
-      />
+
     }
 
     return null
   }
 
-  return <li className='flex gap-2 p-3'>
+  return <li className='flex gap-2 p-3 hover:cursor-pointer hover:text-title-gray transition-colors duration-200 ease-in-out'>
     <Image
       src={icon}
       alt={title}
@@ -34,7 +30,7 @@ const MenuItem: FC<Props> = ({
     />
     <span>{title}</span>
 
-    {renderArrowIcon()}
+    {items && <ArrowIcon className="group-hover:fill-title-gray" />}
   </li>
 }
 
