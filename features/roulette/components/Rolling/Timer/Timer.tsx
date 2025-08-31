8f07069f0283814546a17fc73f6b2a50099803a0
mohsenAKB <mohsen.akb1377@gmail.com>
2025-08-31 20:56:11 +0330

    [CREATE]: basic rolling and timer for main page

--- features/roulette/components/Rolling/Timer/Timer.tsx
+++ features/roulette/components/Rolling/Timer/Timer.tsx
@@ -3,7 +3,7 @@ import { FC, ReactNode } from 'react'
 
 const Timer: FC = (): ReactNode => {
   return (
-    <div className='flex flex-col items-center gap-2.5'>
+    <div className='flex flex-col items-center gap-2.5 leading-[29px]'>
       <div className='flex gap-1'>
         <span className='text-body-gray-900 text-[22px] font-semibold'>Rolling</span>
         <span className='text-title-gray text-[22px] font-semibold'>3.2s</span>
