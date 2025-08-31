73eb225bb9f3891a7ae3ce54f2aedcb69a61fb56
mohsenAKB <mohsen.akb1377@gmail.com>
2025-08-31 19:19:38 +0330

    [CREATE]: basic roulette main background

--- features/roulette/components/Roulette.tsx
+++ features/roulette/components/Roulette.tsx
@@ -1,15 +1,25 @@
 import { PrimaryForm } from "@/features/form";
 import { ActionMenu } from "@/shared";
+import Image from "next/image";
 import { FC, ReactNode } from "react";
 
 const Roulette: FC = (): ReactNode => {
   return (
-    <div>
+    // TODO: should remove min height
+    <section className="min-h-[1000px]">
+
+      <Image
+        src="/images/background-image.png"
+        alt="background"
+        fill
+        className="max-h-[788px] z-0 object-cover"
+      />
+
       <div className="px-[48px] my-4">
         <ActionMenu />
         <PrimaryForm />
       </div>
-    </div>
+    </section>
   );
 };
 
--- shared/layouts/DashboardLayout/Header/Header.tsx
+++ shared/layouts/DashboardLayout/Header/Header.tsx
@@ -5,7 +5,7 @@ import Menu from './Menu/Menu'
 
 const Header: FC = (): ReactNode => {
   return (
-    <header className='fixed w-full left-0 top-0 h-[68px] backdrop-blur-[50px] flex items-center justify-between px-5'>
+    <header className='fixed w-full left-0 top-0 h-[68px] backdrop-blur-[50px] flex items-center justify-between px-5 z-[100] bg-[linear-gradient(180deg,rgba(var(--color-surface-100-rgb),0.2)_0%,rgba(var(--color-surface-100-rgb),0)_100%)]'>
       <div className='flex items-center gap-2'>
         <Logo />
         <Menu />
