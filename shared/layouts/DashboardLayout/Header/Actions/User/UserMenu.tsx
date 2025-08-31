7a3424ad72cbba7761af7b9066b6a42b36fba78e
mohsenAKB <mohsen.akb1377@gmail.com>
2025-08-31 05:17:21 +0330

    [CREATE]: badge component and user for user avatar in header

--- shared/layouts/DashboardLayout/Header/Actions/User/UserMenu.tsx
+++ shared/layouts/DashboardLayout/Header/Actions/User/UserMenu.tsx
@@ -1,8 +1,19 @@
+import Badge from "@/shared/components/Badge/Badge"
+import Image from "next/image"
 import { FC, ReactNode } from "react"
 
 const UserMenu: FC = (): ReactNode => {
   return (
-    <div>UserMenu</div>
+    <Badge>
+      <Image
+        className="rounded-full"
+        width={44}
+        height={44}
+        sizes="200px"
+        src="/images/avatar.jpg"
+        alt="avatar"
+      />
+    </Badge>
   )
 }
 
