06459588de65f026248df6aa116ec7c73f6c6f5c
mohsenAKB <mohsen.akb1377@gmail.com>
2025-09-01 05:16:29 +0330

    [CREATE]: rolling progress

--- features/roulette/components/Rolling/Cards/Cards.tsx
+++ features/roulette/components/Rolling/Cards/Cards.tsx
@@ -1,30 +1,22 @@
 "use client";
 
-import { StairsSwiper } from "@/shared";
+import { StairsSwiper, StairsSwiperRef } from "@/shared";
 import { SwiperSlide } from "swiper/react";
 import OptionCard from "./OptionCard/OptionCard";
 import Image from "next/image";
+import { forwardRef } from "react";
 
+type CardsProps = {
+  slideSpeed: number;
+  slideDelay?: number;
+};
 
-const Cards = () => {
-
+const Cards = forwardRef<StairsSwiperRef, CardsProps>(function Cards(
+  { slideSpeed, slideDelay = 900 },
+  ref
+) {
   const items = [
     <OptionCard type="cs" />,
-
-    <OptionCard type="gold" />,
-    <OptionCard type="silver" />,
-    <OptionCard type="gold" />,
-    <OptionCard type="silver" />,
-
-    <OptionCard type="cs" />,
-
-    <OptionCard type="gold" />,
-    <OptionCard type="silver" />,
-    <OptionCard type="gold" />,
-    <OptionCard type="silver" />,
-
-    <OptionCard type="cs" />,
-
     <OptionCard type="gold" />,
     <OptionCard type="silver" />,
     <OptionCard type="gold" />,
@@ -34,37 +26,44 @@ const Cards = () => {
     <OptionCard type="silver" />,
     <OptionCard type="gold" />,
     <OptionCard type="silver" />,
-  ]
+    <OptionCard type="cs" />,
+    <OptionCard type="gold" />,
+    <OptionCard type="silver" />,
+    <OptionCard type="gold" />,
+    <OptionCard type="silver" />,
+    <OptionCard type="cs" />,
+    <OptionCard type="gold" />,
+    <OptionCard type="silver" />,
+    <OptionCard type="gold" />,
+    <OptionCard type="silver" />,
+  ];
 
+  return (
+    <div className="min-h-[354px] flex flex-col items-center gap-3">
+      <Image width={24} height={24} src="/icons/Polygon.svg" alt="arrow" />
 
-  return <div className="min-h-[354px] flex flex-col items-center gap-3">
-    <Image
-      width={24}
-      height={24}
-      src="/icons/Polygon.svg"
-      alt="arrow"
-    />
+      <StairsSwiper
+        gap={12}
+        stepPx={10}
+        bandMult={1}
+        stairsEffect
+        speed={slideSpeed}
+        autoplay={{ delay: slideDelay, disableOnInteraction: false }}
+        allowTouchMove={false}
+        loop
+        centeredSlides
+        slidesPerView="auto"
+        className="[&_.swiper]:min-h-[330px]"
+        ref={ref}
+      >
+        {items.map((item, i) => (
+          <SwiperSlide key={i} className="!h-auto">
+            {item}
+          </SwiperSlide>
+        ))}
+      </StairsSwiper>
+    </div>
+  );
+});
 
-    <StairsSwiper
-      gap={12}
-      stepPx={10}
-      bandMult={1}
-      stairsEffect
-      autoplay={{ delay: 900, disableOnInteraction: false }}
-      allowTouchMove={false}
-      loop
-      centeredSlides
-      slidesPerView="auto"
-      className="[&_.swiper]:min-h-[330px]"
-    >
-      {items.map((item, i) => (
-        <SwiperSlide key={i} className="!h-auto">
-          {item}
-        </SwiperSlide>
-      ))}
-    </StairsSwiper>
-
-  </div>
-}
-
-export default Cards
+export default Cards;
